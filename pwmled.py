8f8d16aa3b28b56a433056961208e1e0be537b78
Daks Wang <dakswang@gmail.com>
2017-07-11 21:08:45 +0800

    更新測試

--- pwmled.py
+++ pwmled.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 #讓紅色LED燈分成0-100等級亮度
+# 豬頭胖胖
 import RPi.GPIO as GPIO
 led_pin = 17
 
@@ -10,6 +11,6 @@ pwm_led = GPIO.PWM(led_pin, 500)
 pwm_led.start(100)
 
 while True:
-   duty_s = raw_input("輸入亮度值(0~100):")
+   duty_s = raw_input("請輸入亮度值(0~100):")
    duty = int(duty_s)
    pwm_led.ChangeDutyCycle(duty)
